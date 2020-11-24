b725524bbe0a337678cc6137d1414b827ac08fef
krambono <krambono@student.42lyon.fr>
2020-11-24 20:39:28 +0100

    mgl

--- mgl/include/mgl.h
+++ mgl/include/mgl.h
@@ -2,6 +2,40 @@
 #define MGL_H
 
 #include "libft.h"
+#include <stdio.h>
+
+typedef float t_mat2x2[2][2];
+typedef float t_mat3x3[3][3];
+typedef float t_mat4x4[4][4];
+
+struct s_vec2
+{
+	float x;
+	float y;
+};
+typedef struct s_vec2 t_vec2;
+
+struct s_vec3
+{
+	float x;
+	float y;
+	float z;
+};
+typedef struct s_vec3 t_vec3;
+
+struct s_vec4
+{
+	float x;
+	float y;
+	float z;
+	float w;
+};
+typedef struct s_vec4 t_vec4;
+
+void print_mat(void *mat, int matSize);
+void print_vec(void *vec, int vecSize);
+void dotmm(void *mat1, void *mat2, void *out, int size);
+void dotmv(void *mat, void *vec, void *out, int size);
 
 void test(void);
 
--- src/scop.c
+++ src/scop.c
@@ -2,6 +2,17 @@
 
 int main(void)
 {
-	test();
+	setbuf(stdout, NULL);
+	t_mat3x3 matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+	t_vec3   vec = { 1, 2, 3 };
+	t_vec3   vecResult;
+
+	print_mat(matrix, 3);
+	print_vec(&vec, 3);
+
+	dotmv(matrix, &vec, &vecResult, 3);
+
+	print_vec(&vecResult, 3);
+
 	return 0;
 }
