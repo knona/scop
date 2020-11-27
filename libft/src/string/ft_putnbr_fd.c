fe76c95a4dbf167da660219674b7ce735140b872
krambono <krambono@student.42lyon.fr>
2020-11-27 19:16:44 +0100

    update libft

--- libft/src/string/ft_putnbr_fd.c
+++ libft/src/string/ft_putnbr_fd.c
@@ -1,17 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: krambono <krambono@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/02 13:47:23 by krambono          #+#    #+#             */
-/*   Updated: 2020/11/27 17:36:45 by krambono         ###   ########lyon.fr   */
+/*   Created: 2020/11/27 18:48:34 by krambono          #+#    #+#             */
+/*   Updated: 2020/11/27 19:11:43 by krambono         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalnum(int c)
+#include "libft.h"
+
+static void	recursive(int nb, int fd)
 {
-	return ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')
-			|| (c <= '9' && c >= '0'));
+	if (nb == 0)
+		return ;
+	recursive(nb / 10, fd);
+	ft_putchar_fd(nb % 10 + '0', fd);
+}
+
+void		ft_putnbr_fd(int nb, int fd)
+{
+	if (nb == 0)
+		ft_putchar_fd('0', fd);
+	else if (nb == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
+	{
+		if (nb < 0)
+		{
+			ft_putchar_fd('-', fd);
+			recursive(-nb, fd);
+		}
+		else
+			recursive(nb, fd);
+	}
 }
